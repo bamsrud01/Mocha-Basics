9b8754b4f5d13fcea4e9fbb045b00a36f23b0dd6
Barrett Amsrud <barrettamsrud@gmail.com>
2017-07-19 22:29:42 -0500

    Added more test cases

--- 01-treehouse/textUtilities.js
+++ 01-treehouse/textUtilities.js
@@ -1,11 +1,17 @@
 //  Import from Chai library
 const expect = require('chai').expect;
 
-//  Chai comparison
-expect(true).to.be.true;
-
 function titleCase(title) {
-  return title;
+  const words = title.split(' ');
+  let titleCasedWords = words.map(function(word) {
+    return word[0].toUpperCase() + word.substring(1);
+  });
+  
+  return titleCasedWords.join(' ');
 }
 
+//  Write a suite of tests from simplest to most complex
 expect(titleCase('the great mouse detective')).to.be.a('string');
+expect(titleCase('a')).to.equal('A');
+expect(titleCase('vertigo')).to.equal('Vertigo');
+expect(titleCase('the great mouse detective')).to.equal('The Great Mouse Detective');
