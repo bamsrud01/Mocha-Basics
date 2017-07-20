8f573491af61fdcf37ba823ceae2968cad37d748
Barrett Amsrud <barrettamsrud@gmail.com>
2017-07-20 16:41:13 -0500

    Began using Mocha

---
+++ 01-treehouse/test/main_test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+//  Set up test suite (block of closely-related tests)
+describe('Mocha', function() {
+  //  Test spec (an individual test in a suite)
+  it('should run tests using npm', function() {
+    expect(true).to.be.ok;
+  });
+});
