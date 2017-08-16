5029fd7d1ce343512fb9e1ec0f114166f92da675
Barrett Amsrud <barrettamsrud@gmail.com>
2017-08-15 20:58:47 -0500

    Added pending tests

---
+++ 01-treehouse/test/game_test.js
@@ -0,0 +1,10 @@
+//  Import chai.expect
+var expect = require('chai').expect;
+
+//  Test suite for Game Instance Functions
+describe('GAME INSTANCE FUNCTIONS', function() {
+  describe('checkGameStatus', function() {
+    it('should tell me when the game is over');
+  });
+  xit('Some other stuff', function() {});
+});
