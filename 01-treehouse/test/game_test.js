404dc7b5aac22d4b80dd201db553f806c9b27f2a
Barrett Amsrud <barrettamsrud@gmail.com>
2017-08-15 21:36:11 -0500

    Added asynchronous tests

--- 01-treehouse/test/game_test.js
+++ 01-treehouse/test/game_test.js
@@ -2,9 +2,62 @@
 var expect = require('chai').expect;
 
 //  Test suite for Game Instance Functions
-describe('GAME INSTANCE FUNCTIONS', function() {
-  describe('checkGameStatus', function() {
-    it('should tell me when the game is over');
+describe('GAME INSTANCE FUNCTIONS', function () {
+  describe('checkGameStatus', function () {
+    var checkGameStatus = require('../game_logic/game_instance.js').checkGameStatus;
+    it('should tell me when the game is over', function () {
+      var players = [
+        {
+					ships: [
+						{
+							locations: [[0, 0]],
+							damage: [[0, 0]]
+						}
+					]
+        }
+      ];
+      var actual = checkGameStatus(players);
+      expect(actual).to.be.false;
+    });
   });
-  xit('Some other stuff', function() {});
+
+  describe('takeTurn', function() {
+    var takeTurn = require('../game_logic/game_instance').takeTurn;
+    var guess, player;
+
+    beforeEach(function() {
+      guess = function() { return [0, 0] };
+      player = {
+        ships: [
+          {
+            locations: [[0, 0]],
+            damage: []
+          }
+        ]
+      }
+    });
+
+    it('should return false if the game ends', function() {
+      var actual = takeTurn(player, guess);
+      expect(actual).to.be.false;
+    });
+  });
+
+  function saveGame(callback) {
+    setTimeout(function() {
+      callback();
+    }, 1000);
+  }
+
+  describe('saveGame', function() {
+    it('should update save status', function(done) {
+      var status = 'game not saved';
+      saveGame(function() {
+        status = 'game saved';
+        expect(status).to.equal('game saved');
+        done();
+      });
+    });
+  });
+
 });
